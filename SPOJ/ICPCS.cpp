67ae60d30f5a18010177847a19656aa4fdaa55d4
ayaNader <30926124+ayaNader@users.noreply.github.com>
2017-10-12 17:44:13 +0200

    Update ICPCS.cpp

--- SPOJ/ICPCS.cpp
+++ SPOJ/ICPCS.cpp
@@ -1,10 +1,16 @@
+/*
+  idea:
+  - saving the ranking when ep=20 and keeping the realation between every pair with equal number of problems
+  - then trying all possible ep's and minimizing and maximizing the ones that dont change the rank
+*/
 #include<bits/stdc++.h>
 using namespace std;
 
 int tp[105];
 int fa[105];
 int solved[105];
-
+vector<pair<pair<int,int>,int>>v;
+bool chng[105][105];
 int t,p;
 int stl(string s){
 	int ret=0;
@@ -15,6 +21,22 @@ int stl(string s){
 	return ret;
 }
 
+bool chk(int ep){
+	bool val=true;
+	for(int i=0;i<t-1;i++){
+		if(v[i].first.first==v[i+1].first.first){
+			int ind1=v[i].second;
+			int ind2=v[i+1].second;
+			int tmp1=tp[ind1]+ep*fa[ind1];
+			int tmp2=tp[ind2]+ep*fa[ind2];
+			if((tmp1<tmp2&&chng[i][i+1]==0)||(tmp1==tmp2&&chng[i][i+1]==1))
+				continue;
+			val=false;
+			break;
+		}
+	}
+	return val;
+}
 int main(){
 	ios_base::sync_with_stdio(0);
 
@@ -38,14 +60,14 @@ int main(){
 				solved[i]++;
 			}
 		}
-		vector<pair<pair<int,int>,int>>v(t);
+		v.resize(t);
 		for(int i=0;i<t;i++){
 			v[i].first.first=solved[i];
 			v[i].first.second=-(tp[i]+20*fa[i]);
 			v[i].second=i;
 		}
 		sort(v.rbegin(),v.rend());
-		bool chng[105][105];
+
 		for(int i=0;i<t-1;i++){
 			if(v[i].first.first==v[i+1].first.first){
 				int tmp1=-v[i].first.second;
@@ -58,19 +80,7 @@ int main(){
 		}
 		int mi=20;
 		for(int ep=1;ep<=20;ep++){
-			bool val=true;
-			for(int i=0;i<t-1;i++){
-				if(v[i].first.first==v[i+1].first.first){
-					int ind1=v[i].second;
-					int ind2=v[i+1].second;
-					int tmp1=tp[ind1]+ep*fa[ind1];
-					int tmp2=tp[ind2]+ep*fa[ind2];
-					if((tmp1<tmp2&&chng[i][i+1]==0)||(tmp1==tmp2&&chng[i][i+1]==1))
-						continue;
-					val=false;
-					break;
-				}
-			}
+			bool val=chk(ep);
 			if(!val)
 				continue;
 			mi=ep;
@@ -79,19 +89,7 @@ int main(){
 		int mx=42;
 		bool inf=true;
 		for(int ep=3000;ep>=20;ep--){
-			bool val=true;
-			for(int i=0;i<t-1;i++){
-				if(v[i].first.first==v[i+1].first.first){
-					int ind1=v[i].second;
-					int ind2=v[i+1].second;
-					int tmp1=tp[ind1]+ep*fa[ind1];
-					int tmp2=tp[ind2]+ep*fa[ind2];
-					if((tmp1<tmp2&&chng[i][i+1]==0)||(tmp1==tmp2&&chng[i][i+1]==1))
-						continue;
-					val=false;
-					break;
-				}
-			}
+			bool val=chk(ep);
 			if(!val)
 				continue;
 			inf=false;
